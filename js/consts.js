cc7fb62a877d2774d0f09707dfc2e34f2fee7d94
BeataBarbara <skorusbeata@gmail.com>
2021-03-14 22:43:22 +0100

    first commit

---
+++ js/consts.js
@@ -0,0 +1,9 @@
+const mainTitle = "Olejki eteryczne"
+const subPageTitle1 = "Olejek lawendowy"
+const subPageTitle2 = "Olejek z drzewa herbacianego"
+
+module.exports = {
+    mainTitle,
+    subPageTitle1,
+    subPageTitle2
+}
